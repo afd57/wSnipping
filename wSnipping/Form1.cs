5109a7ac866c3721660983608999298439e48ed7
Aziz <afdagli@gmail.com>
2018-07-17 20:31:01 +0300

    Snip selected windows tool. Version 1 Developed By afdagl!

---
+++ wSnipping/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace wSnipping
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int i = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ScreenCapture sc = new ScreenCapture();
+            // capture entire screen, and save it to a file
+            Image img = sc.CaptureScreen();
+            // display image in a Picture control named imageDisplay
+            this.imageDisplay.Image = img;
+            // capture this window, and save it
+            IntPtr handle = GetForegroundWindow();
+            sc.CaptureWindowToFile(handle, "C:\\Test\\temp" + i.ToString() + ".png", ImageFormat.Png);
+            i++;
+            label1.Text = handle.ToString();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                MessageBox.Show("Test");
+            }
+        }
+    }
+}
